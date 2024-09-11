cffc4ad9023e3e158fd440de7c8ee708be432e6b
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-09-11 16:20:31 +0200

    Parametrize map visualization (#10)
    * Parametrize zoom in map visualization
    
    * Parametrize tiles

--- src/GoogleHashCode2014.jl
+++ src/GoogleHashCode2014.jl
@@ -28,10 +28,17 @@ include("random_walk.jl")
 """
     plot_streets(
         city::City, solution::Union{Solution,Nothing}=nothing;
-        path::Union{AbstractString,Nothing}=nothing
+        path::Union{AbstractString,Nothing}=nothing,
+        tiles::AbstractString="Cartodb Positron",
+        zoom_start::Integer=12,
     )
 
-Plot a [`City`](@ref) and an optional [`Solution`](@ref) using the Python library [folium](https://python-visualization.github.io/folium/), save the result as an HTML file at `path`
+Plot a [`City`](@ref) and an optional [`Solution`](@ref) using the Python library [`folium`](https://python-visualization.github.io/folium/), save the result as an HTML file at `path`.
+
+Additional parameters passed to `folium` include:
+
+- The set of `tiles` used to decorate the map
+- The initial zoom level `zoom_start`
 
 This method is defined in a package extension and requires PythonCall.jl to be loaded.
 """
